9b2b72ff8e0d9cd49eb2166a29a211d519bdcdae
Annwyn <annwyn.maloney@mths.ca>
2025-03-03 09:42:26 -0500

    Create code.py

---
+++ code.py
@@ -0,0 +1,23 @@
+"""
+Created by Annwyn M. on Mar 3/2025 
+this makes an LED blink every second on a breadboard 
+"""
+
+import time
+import board 
+import digitalio 
+
+led = digitalio.DigitalInOut(board.GP5) 
+led.direction = digitalio.Direction.OUTPUT
+
+blink_time = 1 
+
+# loop forever 
+while True: 
+  print(blink_time) 
+  led.value = Ture #turnd LED on 
+  time.sleep(blink_time) #wait for 1000ms 
+  led.value = False #turns LED off 
+  time.sleep(blink_time) #wait for 1000ms
+
+  blink_time += 1 
